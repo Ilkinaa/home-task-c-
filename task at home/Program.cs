474e02c9e710c9594b8367d3d503dc38acbb1e42
Ilkinaa <ilkinba@code.edu.az>
2023-02-27 23:19:28 +0400

    done

---
+++ task at home/Program.cs
@@ -0,0 +1,33 @@
+﻿//task1
+//int[] arr = { 1, 3, 4, 7, 88, 42 };
+
+
+//    {
+//        Console.WriteLine("var") ;
+//    }
+//Sum(4);
+//void Sum(int num)
+//{
+//    for (int i = 0; i < arr.Length; i++)
+//    {
+//        if (arr[i] == num)
+//        {
+//            Console.WriteLine("Var");
+//        }
+//    }
+//}
+
+//task2
+//int ArrayMax(params int[] numbers)
+//{
+//    int max = numbers[0];
+//    foreach (int items in numbers)
+//    {
+//        if (items > max)
+//        {
+//            max = items;
+//        }
+//    }
+//    return max;
+//}
+//Console.WriteLine("Maximum Of Array : " +ArrayMax(1,4,6,443,556,778,15,78,55,45,78));
